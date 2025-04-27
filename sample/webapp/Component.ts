fbe4f76376a47847e8d19a9742cd706f516d5caf
lemaiwo <wouterlemaire120@hotmail.com>
2025-04-27 23:24:54 +0200

    third party library

--- sample/webapp/Component.ts
+++ sample/webapp/Component.ts
@@ -1,5 +1,7 @@
 import BaseComponent from "sap/ui/core/UIComponent";
 import { createDeviceModel } from "./model/models";
+import Decimal from "decimal.js";
+import MessageBox from "sap/m/MessageBox";
 
 /**
  * @namespace be.wl.tooling.fiori.sample
@@ -19,7 +21,12 @@ export default class Component extends BaseComponent {
 
         // set the device model
         this.setModel(createDeviceModel(), "device");
-
+        const x = new Decimal(123.4567),
+            y = new Decimal('123456.7e-3'),
+            z = new Decimal(x);
+        if(x.equals(y) && y.equals(z) && x.equals(z)){
+            MessageBox.show("Equal ");
+        } 
         // enable routing
         this.getRouter().initialize();
 	}
