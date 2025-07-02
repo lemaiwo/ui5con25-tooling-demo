1a373d691dd37fd7b4c87fdee17f222176822932
lemaiwo <wouterlemaire120@hotmail.com>
2025-07-02 21:00:58 +0200

    Add new CDN configuration and update OData version; refactor component initialization

--- sample/webapp/Component.ts
+++ sample/webapp/Component.ts
@@ -1,6 +1,6 @@
 import BaseComponent from "sap/ui/core/UIComponent";
 import { createDeviceModel } from "./model/models";
-import Decimal from "decimal.js";
+// import Decimal from "decimal.js";
 import MessageBox from "sap/m/MessageBox";
 
 /**
@@ -8,26 +8,26 @@ import MessageBox from "sap/m/MessageBox";
  */
 export default class Component extends BaseComponent {
 
-	public static metadata = {
-		manifest: "json",
+    public static metadata = {
+        manifest: "json",
         interfaces: [
             "sap.ui.core.IAsyncContentCreation"
         ]
-	};
+    };
 
-	public init() : void {
-		// call the base component's init function
-		super.init();
+    public init(): void {
+        // call the base component's init function
+        super.init();
 
         // set the device model
         this.setModel(createDeviceModel(), "device");
-        const x = new Decimal(123.4567),
-            y = new Decimal('123456.7e-3'),
-            z = new Decimal(x);
-        if(x.equals(y) && y.equals(z) && x.equals(z)){
-            MessageBox.show("Equal ");
-        } 
+        // const x = new Decimal(123.4567),
+        //     y = new Decimal('123456.7e-3'),
+        //     z = new Decimal(x);
+        // if (x.equals(y) && y.equals(z) && x.equals(z)) {
+        //     MessageBox.show("Equal ");
+        // }
         // enable routing
         this.getRouter().initialize();
-	}
+    }
 }
